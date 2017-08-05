0f59ed52fe54e4b3b11be390ea842e9d3de6a24c
Muriel Silveira <murielkong@gmail.com>
2017-08-05 15:09:11 +0200

    mix new jurassic

---
+++ lib/jurassic.ex
@@ -0,0 +1,18 @@
+defmodule Jurassic do
+  @moduledoc """
+  Documentation for Jurassic.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jurassic.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jurassic_test.exs
@@ -0,0 +1,8 @@
+defmodule JurassicTest do
+  use ExUnit.Case
+  doctest Jurassic
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
